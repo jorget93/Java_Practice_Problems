c31b2af66a871ef4579d0f43782fa7e272d5ceda
jorget93 <jorgetapia0293@gmail.com>
2021-06-02 13:18:01 -0400

    first test passed

--- src/main/java/Practice_Problems.java
+++ src/main/java/Practice_Problems.java
@@ -1,2 +1,18 @@
 public class Practice_Problems {
+
+    //Write a method to check if a given string is a palindrome.
+    public boolean palindromeCheck(String someString){
+        boolean stringCheck = false;
+        int length = someString.length() - 1;
+        for(int i = 0, j = length; i <= length; i++){
+            if(someString.charAt(i) != someString.charAt(j)){
+                return false;
+            }
+            j--;
+        }
+        return true;
+    }
+
+
+
 }
---
+++ src/test/java/PalindromeCheckTests.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PalindromeCheckTests {
+    Practice_Problems practice = new Practice_Problems();
+
+    @Test
+    public void palindromeCheckTest1(){
+        //arrange
+        String someString = "aba";
+        //action
+        boolean palindromeCheck = practice.palindromeCheck(someString);
+        //assert
+        Assert.assertTrue(palindromeCheck);
+    }
+}
