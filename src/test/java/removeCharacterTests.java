c289071c4cfd1c72c914402507b3f9109717dd07
jorget93 <jorgetapia0293@gmail.com>
2021-06-02 13:37:27 -0400

    second test passed

--- src/test/java/removeCharacterTests.java
+++ src/test/java/removeCharacterTests.java
@@ -5,9 +5,21 @@ public class removeCharacterTests {
     Practice_Problems practice = new Practice_Problems();
 
     @Test
-    public void removeCharacterTests(){
+    public void removeCharacterTest1(){
         //arrange
         String someString = "cats";
+        char characterToRemove = 's';
+        //action
+        String expected = "cat";
+        String actual = practice.removeCharacter(someString, characterToRemove);
+        //assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeCharacterTest2(){
+        //arrange
+        String someString = "casts";
         char characterToRemove = 's';
         //action
         String expected = "cat";
