91b3c423800d3d5d38bfc8b930830b8babde1119
jorget93 <jorgetapia0293@gmail.com>
2021-06-11 10:04:18 -0400

    third test passed test passed

--- src/test/java/binarySearchTests.java
+++ src/test/java/binarySearchTests.java
@@ -15,4 +15,28 @@ public class binarySearchTests {
         //assert
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void binarySearchTest2(){
+        //arrange
+        int[] sortedArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        int low = 1, high = 13, key = 11;
+        //action
+        int expected = 11;
+        int actual = practice.binarySearch(sortedArray, low, high, key);
+        //assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchTest3(){
+        //arrange
+        int[] sortedArray = { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        int low = 7, high = 17, key = 11;
+        //action
+        int expected = 0;
+        int actual = practice.binarySearch(sortedArray, low, high, key);
+        //assert
+        Assert.assertEquals(expected, actual);
+    }
 }
