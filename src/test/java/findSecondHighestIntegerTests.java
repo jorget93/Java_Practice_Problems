58785c995974cd26eb01fce3bbe3651f1a1c634c
jorget93 <jorgetapia0293@gmail.com>
2021-06-16 14:25:18 -0400

    test case one passed

--- src/test/java/findSecondHighestIntegerTests.java
+++ src/test/java/findSecondHighestIntegerTests.java
@@ -1,2 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class findSecondHighestIntegerTests {
+    Practice_Problems practice = new Practice_Problems();
+
+    @Test
+    public void findSecondHighestIntegerTest1(){
+        //arrange
+        Integer[] someArray = { 2, 1, 5, 6, 4, 9};
+
+        //action
+        Integer expected = 6;
+        Integer actual = practice.findSecondHighestInteger( someArray );
+
+        //assert
+        Assert.assertEquals( expected, actual);
+    }
+
 }
