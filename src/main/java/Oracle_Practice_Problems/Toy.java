8edb26e5be370b9cbb44d8b8ca5a86433f2790e9
jorget93 <jorgetapia0293@gmail.com>
2021-07-23 19:05:05 -0400

    added another class to analyze

---
+++ src/main/java/Oracle_Practice_Problems/Toy.java
@@ -0,0 +1,21 @@
+package Oracle_Practice_Problems;
+
+class Toy{
+    private boolean containsIce = false;
+    public boolean containsIce() {
+        return containsIce;
+    }
+    public void removeIce() {
+        this.containsIce = true;
+    }
+}
+public class Otter {
+    private static void play(Toy toy) {
+        toy.removeIce();
+    }
+    public static void main(String[] args) {
+        Toy toy = new Toy();
+        Otter.play(toy);
+        System.out.println(toy.containsIce());
+    }
+}
